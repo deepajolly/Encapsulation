19dcf52262b50f56e91b494ded549c3f6432daeb
deepajolly <63454512+deepajolly@users.noreply.github.com>
2020-08-03 05:57:51 -0700

    uploading my project to repository

---
+++ Encapsulation/src/example/Test.java
@@ -0,0 +1,12 @@
+package example;
+
+public class Test {
+	public static void main(String args[])
+	{
+		Student s=new Student();
+		s.setName("Deepa");
+		System.out.println(s.getName());
+	}
+	}
+
+
